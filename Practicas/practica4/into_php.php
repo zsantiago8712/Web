c55f96d7b0c100d9c824134f65ab1808a87695ab
Santiago Zamora <zsantiago81@icloud.com>
2023-02-08 11:17:21 -0600

    practica 4 y 5

---
+++ Practicas/practica4/into_php.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>INtro a PHP</h1>
+    <?php
+    echo $nombre = "Santiago";
+        echo "<br>";
+        echo "<h3>" . $nombre . "</h3>";
+    ?>
+
+    <h5> <?= $nombre ?> </h5>
+</body>
+
+</html>
