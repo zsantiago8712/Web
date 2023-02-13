4f7f1ee694266a09603073c125a338fc643c04ca
Santiago Zamora <zsantiago81@icloud.com>
2023-02-13 08:01:45 -0600

    practica 6

---
+++ Practicas/practica6/login.php
@@ -0,0 +1,7 @@
+<?php
+$miVariable = 15;
+echo "<h3>Contenido de miVariable $miVariable</h3>";
+var_dump($miVariable);
+echo "<br>";
+echo "<h3>Contenido de la variable $_GET</h3>";
+var_dump($_GET);
---
+++ Practicas/practica6/login2.php
@@ -0,0 +1,8 @@
+<?php
+$miVariable= 15;
+echo "<h3>Contenido de miVariable $miVariable</h3>";
+var_dump($miVariable);
+echo "<br>";
+echo "<h3>Contenido de la variable $_POST</h3>";
+var_dump($_POST);
+?>
