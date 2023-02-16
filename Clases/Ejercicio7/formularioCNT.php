df60b849173d6f386d0a1e6926cebf0a9bb85718
Santiago Zamora <zsantiago81@icloud.com>
2023-02-16 08:16:21 -0600

    coorecciones

--- Clases/Ejercicio7/formularioCNT.php
+++ Clases/Ejercicio7/formularioCNT.php
@@ -9,7 +9,7 @@ if ($_POST['operacion'] == "Suma") {
 } elseif ($_POST['operacion'] == "Resta") {
     $res = (int)$_POST['variable1'] - (int)$_POST['variable2'];
     echo $_POST['variable1'] . "-" . $_POST['variable2'] . "= $res";
-}elseif ($_POST['operacion'] == "Multicacion") {
+}elseif ($_POST['operacion'] == "Multiplicacion") {
     $res = (int)$_POST['variable1'] * (int)$_POST['variable2'];
     echo $_POST['variable1'] . "*" . $_POST['variable2'] . "= $res";
 }elseif ($_POST['operacion'] == "Division") {
