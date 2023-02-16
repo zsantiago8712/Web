7196b9fa030fc23b703a261cf05390e6b41de1ec
Santiago Zamora <zsantiago81@icloud.com>
2023-02-16 07:56:08 -0600

    correciones

--- Clases/Ejercicio7/formularioCNT.php
+++ Clases/Ejercicio7/formularioCNT.php
@@ -4,15 +4,15 @@
 var_dump($_POST);
 
 if ($_POST['operacion'] == "Suma") {
-    $res = (int)$_POST['var1'] + (int)$_POST['var2'];
+    $res = (int)$_POST['variable1'] + (int)$_POST['variable2'];
     echo "\nResultado = $res";
 } elseif ($_POST['operacion'] == "Resta") {
-    $res = (int)$_POST['var1'] - (int)$_POST['var2'];
+    $res = (int)$_POST['variable1'] - (int)$_POST['variable2'];
     echo "\nResultado = $res";
 }elseif ($_POST['operacion'] == "Multicacion") {
-    $res = (int)$_POST['var1'] * (int)$_POST['var2'];
+    $res = (int)$_POST['variable1'] * (int)$_POST['variable2'];
     echo "\nResultado = $res";
 }elseif ($_POST['operacion'] == "Division") {
-    $res = (int)$_POST['var1'] / (int)$_POST['var2'];
+    $res = (int)$_POST['variable1'] / (int)$_POST['variable2'];
     echo "\nResultado = $res";
 }
