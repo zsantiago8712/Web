0e72f312921e69ed51d6a321e5e527f464da56ef
Santiago Zamora <zsantiago81@icloud.com>
2023-02-16 08:04:20 -0600

    coorecciones

--- Clases/Ejercicio7/formularioCNT.php
+++ Clases/Ejercicio7/formularioCNT.php
@@ -1,18 +1,18 @@
 <?php
 
 
-var_dump($_POST);
+
 
 if ($_POST['operacion'] == "Suma") {
     $res = (int)$_POST['variable1'] + (int)$_POST['variable2'];
-    echo "\n$_POST[0] + $_POST[2] = $res";
+    echo $_POST['variable1'] . "+" . $_POST['variable2'] . "= $res";
 } elseif ($_POST['operacion'] == "Resta") {
     $res = (int)$_POST['variable1'] - (int)$_POST['variable2'];
-    echo "\n$_POST[0] - $_POST[2] = $res";
+    echo $_POST['variable1'] . "+" . $_POST['variable2'] . "= $res";
 }elseif ($_POST['operacion'] == "Multicacion") {
     $res = (int)$_POST['variable1'] * (int)$_POST['variable2'];
-    echo "\n$_POST[0] * $_POST[2] = $res";
+    echo $_POST['variable1'] . "+" . $_POST['variable2'] . "= $res";
 }elseif ($_POST['operacion'] == "Division") {
     $res = (int)$_POST['variable1'] / (int)$_POST['variable2'];
-    echo "\n$_POST[0] / $_POST[2] = $res";
+    echo $_POST['variable1'] . "+" . $_POST['variable2'] . "= $res";
 }
