a48064fb1f278d493121e2266f4c2600f9675482
Santiago Zamora <zsantiago81@icloud.com>
2023-02-21 08:04:49 -0600

    corr

--- Practicas/Practica4/registro_CTL.php
+++ Practicas/Practica4/registro_CTL.php
@@ -53,6 +53,10 @@ function editUser($editUser)
             $data = implode(",", array($editUser['nombre'], $editUser['apellido'], $editUser['correo']));
         }
    
+        if ($key != count($users) - 1) {
+            $data = $data . "\n";
+        }
+        
         fwrite($file, $data);
     }
     
