2b483099e395c8a171dd669a64277d2cc08eb67c
Santiago Zamora <zsantiago81@icloud.com>
2023-02-27 08:12:38 -0600

    test

--- Clases/Ejercicio9/contenido_secreto.php
+++ Clases/Ejercicio9/contenido_secreto.php
@@ -1,5 +1,6 @@
 <?php 
 
+    var_dump($_SESSION);
     if (isset($_SESSION["user"])) {
         echo "Usuario: " . $_SESSION["user"] . "Validad\n";
     }else {
--- Clases/Ejercicio9/login_CTL.php
+++ Clases/Ejercicio9/login_CTL.php
@@ -3,6 +3,7 @@
 session_start();
 if (!empty($_POST)) {
 
+    var_dump($_POST);
     if ($_POST["username"] == "admin" && $_POST["password"] == "Admin") {
         $_SESSION["user"] = $_POST["username"];
     }
