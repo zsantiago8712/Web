744dee4fe7bb79bb05ae744f36c76d5e7e63ea5d
Santiago Zamora <zsantiago81@icloud.com>
2023-02-27 08:43:23 -0600

    corrcciones

--- Clases/Ejercicio9/login_CTL.php
+++ Clases/Ejercicio9/login_CTL.php
@@ -3,13 +3,13 @@
 
 if (!empty($_POST)) {
 
-    if (!$_POST["username"] == "admin" || !$_POST["password"] == "Admin") {
+
+    if ($_POST["username"] != "admin" || $_POST["password"] != "Admin") {
         echo "Usuario: " . $_POST["username"] . " Invalido\n";
         exit();
     }
     
     session_start();
     $_SESSION["user"] = $_POST["username"];
-
+    header("Location: contenido_secreto.php");
 }
-header("Location: contenido_secreto.php");
