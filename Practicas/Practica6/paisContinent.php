2d9258a3c83d89cfbcb5bfb51e4de9a73cc26d4f
Santiago Zamora <zsantiago81@icloud.com>
2023-03-23 08:47:22 -0600

    practica 6

--- Practicas/Practica6/paisContinent.php
+++ Practicas/Practica6/paisContinent.php
@@ -1,26 +1,45 @@
 <?php
     require_once("../includes/Db.php");
-    $contiente = 4;
-
-    $query = "SELECT Code, Name, Capital, HeadOfState as presidente FROM world.country WHERE continent = ?";
     $db = new Db("localhost", 'dzamora', 'dzamora', '217924', '3306');
-    $db->connect();
-    $rows = $db->prepare($query, $contiente);
+
+    $contients =$db->query("SELECT * FROM world.continent");
+    if (isset($_POST['id_continent'])) {
+        $query = "SELECT Code, Name, Capital, HeadOfState as presidente FROM world.country WHERE continent = ?";
+        $rows = $db->prepare($query, $_POST['id_continent']);
+    }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
     <title>Document</title>
 </head>
 
 <body>
+
+    <div>
+        <form action="paisContinent.php" method="POST" enctype="application/x-www-form-urlencoded">
+            <div>
+                <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example"
+                    name="id_continent">
+                    <option selected>Open this select menu</option>
+                    <? foreach ($contients as $contient) { ?>
+                    <option value="<?= $contient['id_continent'] ?>"> <?= $contient['continent'] ?> </option>
+                    <? } ?>
+                </select>
+            </div>
+            <div>
+                <input type="submit" name="enviar" id="enviar" value="Enviar">
+            </div>
+
+        </form>
+    </div>
     <div class="table-responsive">
         <table class="table caption-top">
             <tr>
@@ -40,6 +59,7 @@
             <? } ?>
         </table>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
